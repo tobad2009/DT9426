33b09eb412099cd3439eb63375647d697271528d
tobad2009 <43885026+tobad2009@users.noreply.github.com>
2019-01-09 12:37:08 +0000

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,7 @@
+import os
+conversion_str = 'C:\\Users\\yashm\\AppData\\Local\\Programs\\Python\\Python36\\Scripts\\pyuic5.exe ' \
+ '-x C:\\Users\\yashm\\Downloads\\Project2018\\gui.ui ' \
+ '-o C:\\Users\\yashm\\Downloads\\Project2018\\gui.py'
+
+
+os.system(conversion_str)
